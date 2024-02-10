c2d516e42cbd7bf06f35eebe3261eff7ee95a3eb
mahmoud1512 <120992403+mahmoud1512@users.noreply.github.com>
2024-02-10 21:07:42 +0200

    basic game file

---
+++ game/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+<div id="game">
+
+  <h1 style="padding-top:30px;">Mahmoud'S REACTION TIMER GAME</h1> 
+
+  <p id="motive">keep your concentration high</p>
+  <button id="start" @click="start()" :disabled="this.playing" > start game </button>
+  <block v-if="this.playing" :del="delay" />
+</div>
+
+
+
+</template>
+
+<script>
+import block from './components/block.vue'
+export default {
+  name: 'App',
+  data(){
+    return{
+      playing:false,
+      delay:0,
+    }
+  },
+
+  components:{
+    block
+  },
+  methods:
+  {
+    start()
+    {
+       this.delay=1000+Math.random()*7000
+      this.playing=true;
+     
+     // console.log(this.delay);
+    }
+  }
+ 
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+ 
+ 
+}
+#app h1{
+   font-size: 2em;
+   color: red;
+
+   
+}
+#title
+{
+  height: 30px;
+  width: 40%;
+  border: solid;
+  border-radius: 20px;
+  margin-left: 30%;
+  color: aqua;
+  opacity: 70%;
+  padding-top: 20px;
+  
+}
+
+#motive
+{
+  color: blueviolet;
+  font-size: 1.25em;
+
+}
+
+#start{
+  height: 40px;
+  width: 90px;
+  border-radius:20px ;
+  background-color: orange;
+}
+
+
+</style>
---
+++ game/src/components/block.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="block" v-if="this.show" @click="stop()">
+    CLICK ME FAST
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['del'],
+    data(){
+        return{
+            show:false,
+            timer:null,
+            interval:0
+            
+        }
+    },
+    mounted()
+    {
+        console.log(this.del)
+         setTimeout(()=>{
+          this.show=true
+           this.start();
+         },this.del);
+       
+    },
+
+
+
+    methods:
+    {
+        start()
+        {
+             this.timer=setInterval(()=>{this.interval+=10},10)
+        },
+        stop()
+        {
+             clearInterval(this.timer);
+             console.log(this.interval);
+             this.interval=0;    
+        }
+    }
+
+       
+}
+</script>
+
+<style>
+
+   .block
+   {
+        width: 400px;
+        border-radius: 20px;
+        background-color: #0faf87;
+        color:black;
+        text-align: center;
+        padding: 100px 0;
+        margin:40px auto;
+   }
+
+</style>
